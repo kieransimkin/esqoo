644a6e36f4ea1bd54fc41e133754590269c1a358
Kieran Simkin <kieran@slinq.com>
2012-02-20 09:19:32 +0000

    test

--- classes/class.MVC.php
+++ classes/class.MVC.php
@@ -1,6 +1,12 @@
 <?php
 class MVC { 
 	public static function dispatch($uri) { 
+		if ($uri=='/') { 
+			$controller='dashboard';
+			$action='index';
+		}
+		$bits=explode('/',$uri);
+		var_dump($bits);
 		print $uri;
 	}
 }
