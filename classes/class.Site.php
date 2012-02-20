b3f2a0394135b487edb5086c53fe46d24ff2bddd
Kieran Simkin <kieran@slinq.com>
2012-02-20 10:07:28 +0000

    fix

--- classes/class.Site.php
+++ classes/class.Site.php
@@ -1,10 +1,15 @@
 <?php
 class Site { 
    private static $instance;
+   public static $approot;
+   function __construct() { 
+	$this->approot=dirname(__FILE__);
+   }
    static function get() {
         if (self::$instance)
             return self::$instance;
         self::$instance = new Site;
+	
         return self::$instance;
     }
 
