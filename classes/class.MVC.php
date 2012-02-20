dc05a6d257b11b4bc46d2cfa5cb62a993b7c507c
Kieran Simkin <kieran@slinq.com>
2012-02-20 09:24:03 +0000

    test

--- classes/class.MVC.php
+++ classes/class.MVC.php
@@ -5,6 +5,9 @@ class MVC {
 			$controller='dashboard';
 			$action='index';
 		}
+		if (substr($uri,0,1)=='/') { 
+			$uri=substr($uri,1);
+		}
 		$bits=explode('/',$uri);
 		var_dump($bits);
 		print $uri;
