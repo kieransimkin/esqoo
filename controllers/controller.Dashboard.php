d4b6a5b3adc5e2e299ffdcd141fa03828aaccbd9
Kieran Simkin <kieran@slinq.com>
2012-02-20 10:26:39 +0000

    header and footer partials

--- controllers/controller.Dashboard.php
+++ controllers/controller.Dashboard.php
@@ -1,19 +1,5 @@
 <?php
 class DashboardController extends OpenController { 
 	function indexUI() { 
-		print "Hello";
 	}
-	function autoloadJS() {
-		$action = $this->action;
-		if(strpos($action,'..')!== false || strpos($action,'/')!==false) {
-			die("Die hacker!");
-		}
-		// Autoload JS files for this controller and action 
-		if(file_exists(realpath(dirname(__FILE__).'/../js').'/'.$this->controller.'/'.$action.".js") && @$_GET['request_type']!='json') {
-			$this->view->head .= "<script src=\"/js/{$this->controller}/{$action}.js\"></script>";
-		} elseif(file_exists(realpath(dirname(__FILE__).'/../js').'/'.$action.".js") && @$_GET['request_type']!='json') {
-			$this->view->head .= "<script src=\"/js/{$action}.js\"></script>";
-		}
-	}
-
 }
