0ab325bc0e98ac515faebe39108d315678a265eb
Kieran Simkin <kieran@slinq.com>
2012-02-20 10:20:39 +0000

    some view work

--- views/dashboard/view.index.php
+++ views/dashboard/view.index.php
@@ -1,1 +1,11 @@
- 
+<?php
+$this->header(_("Dashboard"));
+?>
+<h1 class="top-elem"><?=_('Account Details');?></h1>
+<div class="fullsize-box main-elem ui-corner-all">
+<br>
+</div>
+
+<?php
+$this->footer();
+?> 
