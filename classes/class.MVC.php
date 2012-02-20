93bc77de461d90f5c8daffce87981d7ced237ba9
Kieran Simkin <kieran@slinq.com>
2012-02-20 09:42:49 +0000

    MVC framework work

--- classes/class.MVC.php
+++ classes/class.MVC.php
@@ -24,7 +24,40 @@ class MVC {
 		print "Action: $action<br>\n";
 		print "Arg: $arg<br>\n";
 		print "Api: ".print_r($api,true)."<br>\n";
-		var_dump($bits);
-		print $uri;
+		$controller_class = ucwords($controller).'Controller';
+		$funcname = strtolower(str_replace('-','',$action)).'Action';
+		if (!class_exists($controller_class)) { 
+			self::throw404($controller_class,$funcname);
+		}
+		$new_controller = new $controller_class($controller, $action);
+		$new_controller->setView($controller.'/view.'.$action.".php");
+		$new_controller->action = $action;
+		$new_controller->controller = strtolower($controller);
+		$new_controller->autoloadJS();
+
+		if (!method_exists($new_controller, $funcname)) { 
+			self::throw404($controller_class, $funcname);
+		}
+		$new_controller->$funcname();
+		$new_controller->render();
 	}
+
+	function throw404($controller_class=null, $funcname=null) {
+		self::render404($controller_class, $funcname);
+		die;
+	}
+
+	function render404($controller_class=null, $funcname=null) {
+		header("HTTP/1.1 404 Not Found");
+		?>
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+<html><head>
+<title>404 Not Found</title>
+</head><body>
+<h1>Not Found</h1>
+<p>The requested URL <?=htmlentities($_SERVER['REQUEST_URI'])?> was not found on this server.</p>
+<?php if ($controller_class and $funcname) { ?><p><em>Unknown method <strong><?=$funcname?> </strong> on <strong><?=$controller_class?></strong></em></p><?php } ?>
+</body></html>
+	<?php }
+
 }
