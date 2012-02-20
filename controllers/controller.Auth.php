f4814a1ecde71781a2905a0c8932b95cabad1ef2
Kieran Simkin <kieran@slinq.com>
2012-02-20 17:24:57 +0000

    add sha256.js

--- controllers/controller.Auth.php
+++ controllers/controller.Auth.php
@@ -7,8 +7,14 @@ class AuthController extends OpenController {
 			$salt=User_salt::create(array('user'=>$user,'salt'=>Helper::randomAlphaNumString(64)));
 			$ret['Salt']=$salt->salt;
 			$ret['UserID']=$salt->user_id;
-			//var_dump($salt->user_id);
 		}
 		return $ret;
+	}
+	function authAPI($arg,$input) { 
+		$ret=array();
+		$user=$this->ensure_api_user($input);
+		if ($this->api_validation_success()) { 
+
+		}
 	}
 }
