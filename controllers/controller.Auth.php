30200b238d1fd4e2e6744c5b913ba34562b18861
Kieran Simkin <kieran@slinq.com>
2012-02-20 16:38:11 +0000

    test

--- controllers/controller.Auth.php
+++ controllers/controller.Auth.php
@@ -3,6 +3,7 @@ class AuthController extends OpenController {
 	function getsaltAPI($arg,$input) { 
 		$ret=array();
 		$user=$this->ensure_api_user($input);
+		var_dump($user);
 		var_dump($input);
 		if ($this->api_validation_success()) { 
 			$salt=User_salt::create(array('user'=>$user,'salt'=>Helper::randomAlphaNumString(64)));
