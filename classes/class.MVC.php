77348aba34002913e153e26b1da2ac5f0ed2db37
Kieran Simkin <kieran@slinq.com>
2012-02-20 09:29:17 +0000

    test

--- classes/class.MVC.php
+++ classes/class.MVC.php
@@ -4,11 +4,26 @@ class MVC {
 		if ($uri=='/') { 
 			$controller='dashboard';
 			$action='index';
+			$arg=null;
+		} else { 
+			if (substr($uri,0,1)=='/') { 
+				$uri=substr($uri,1);
+			}
+			$bits=explode('/',$uri);
+			$controller=$bits[0];
+			$action=$bits[1];
+			if ($bits[2]=='api') { 
+				$api=true;
+				$arg=$bits[3];
+			} else { 
+				$api=false;
+				$arg=$bits[2];
+			}
 		}
-		if (substr($uri,0,1)=='/') { 
-			$uri=substr($uri,1);
-		}
-		$bits=explode('/',$uri);
+		print "Controller: $controller<br>\n";
+		print "Action: $action<br>\n";
+		print "Arg: $arg<br>\n";
+		print "Api: ".print_r($api,true)."<br>\n";
 		var_dump($bits);
 		print $uri;
 	}
