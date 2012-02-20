93d072180dd468cfb857dc69a00b671d03da3ada
Kieran Simkin <kieran@slinq.com>
2012-02-20 11:24:38 +0000

    add jQuery

--- views/partials/view.header.php
+++ views/partials/view.header.php
@@ -13,7 +13,7 @@
 	<link rel="stylesheet" href="/jquery-ui-themes/<?=$this->options['theme']?>/jquery.ui.all.css" />
 	<?php
 	$this->cssManager->display();
-	$this->jsManager->add("jquery-1.6.4.min", "jquery-ui-1.8.2.custom");
+	$this->jsManager->add("jquery-1.7.1.min", "jquery-ui-1.8.17.custom");
     	//$this->jsManager->add("jquery.livequery", "jquery.bgiframe.min", "jquery.tools.min", "flexigrid/flexigrid", "jquery.delayedobserver", "jquery.multiselects-0.3", "jquery.tmpl.1.1.1", "jquery.ui.selectmenu", "ui.checkbox", "jquery.blockUI", "ui.multiselect", "flot/jquery.flot", "flot/jquery.flot.pie", "flot/jquery.flot.selection", "flot/jquery.flot.navigate", "uploadify/swfobject", "uploadify/jquery.uploadify.v2.1.4.min", "superfish/superfish", "superfish/supersubs", "jquery.include", "jquery.timeago", "site", "ui", "tiny_mce/tiny_mce", "date");
 	$this->jsManager->display();
 	foreach ($this->jsFiles as $file)
