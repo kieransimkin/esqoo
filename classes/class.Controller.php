0f1af32fabcd698ed2cb41a6e2d78b4f22f27ce2
Kieran Simkin <kieran@slinq.com>
2012-02-20 14:07:29 +0000

    oops

--- classes/class.Controller.php
+++ classes/class.Controller.php
@@ -49,9 +49,9 @@ class Controller {
 		}
 		// Autoload JS files for this controller and action 
 		if(file_exists(realpath(dirname(__FILE__).'/../js').'/'.$this->controller.'/'.$action.".js") && @$_GET['request_type']!='json') {
-			$this->view->head .= "<script src=\"/js/{$this->controller}/{$action}.js\"></script>";
+			$this->view->head .= "<script src=\"/js/{$this->controller}/{$action}.js\"></script>\n";
 		} elseif(file_exists(realpath(dirname(__FILE__).'/../js').'/'.$action.".js") && @$_GET['request_type']!='json') {
-			$this->view->head .= "<script src=\"/js/{$action}.js\"></script>";
+			$this->view->head .= "<script src=\"/js/{$action}.js\"></script>\n";
 		}
 	}
 	function autoloadCSS() {
@@ -61,9 +61,9 @@ class Controller {
 		}
 		// Autoload JS files for this controller and action 
 		if(file_exists(realpath(dirname(__FILE__).'/../css').'/'.$this->controller.'/'.$action.".css") && @$_GET['request_type']!='json') {
-			$this->view->head .= "<link rel=\"stylesheet\" href=\"/css/{$this->controller}/{$action}.css\" />";
+			$this->view->head .= "<link rel=\"stylesheet\" href=\"/css/{$this->controller}/{$action}.css\" />\n";
 		} elseif(file_exists(realpath(dirname(__FILE__).'/../css').'/'.$action.".css") && @$_GET['request_type']!='json') {
-			$this->view->head .= "<link rel=\"stylesheet\" href=\"/css/{$action}.css\" />";
+			$this->view->head .= "<link rel=\"stylesheet\" href=\"/css/{$action}.css\" />\n";
 		}
 	}
 	function __toString() { 
