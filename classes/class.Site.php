c94f11a2a2723b98b92579923cd4fbf17908a6ac
Kieran Simkin <kieran@slinq.com>
2012-02-20 10:05:27 +0000

    fix

--- classes/class.Site.php
+++ classes/class.Site.php
@@ -1,4 +1,10 @@
 <?php
 class Site { 
+   static function get() {
+        if (self::$instance)
+            return self::$instance;
+        self::$instance = new Site;
+        return self::$instance;
+    }
 
 } 
