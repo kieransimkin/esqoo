2439cd95a949957d3be32c682b5473291ce2d6e3
Kieran Simkin <kieran@slinq.com>
2012-02-21 13:58:25 +0000

    cleanup

--- views/partials/auth/view.loginbox.php
+++ views/partials/auth/view.loginbox.php
@@ -1,8 +1,13 @@
 <?php
-?><section class="login-box">
-<label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
-<label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
-<input type="hidden" id="login-challenge" value="<?=$this->challenge['Challenge'];?>">
-<input type="hidden" id="login-challenge-id" value="<?=$this->challenge['ChallengeID'];?>">
-<input type="button" id="login-button" value="<?=_('Login');?>">
+?>
+<section class="login-box">
+	<label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="<?=_('you@yourdomain.com');?>">
+	<label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="<?=_('******');?>">
+	<input type="hidden" id="login-challenge" value="<?=$this->challenge['Challenge'];?>">
+	<input type="hidden" id="login-forward" value="<?=$this->forward;?>">
+	<input type="hidden" id="login-challenge-id" value="<?=$this->challenge['ChallengeID'];?>">
+	<input type="button" id="login-button" value="<?=_('Login');?>" onclick="esqoo_login(); return false;">
+	<noscript>
+		You will need JavaScript enabled past this point.
+	</noscript>
 </section>
