9a7ff1c64d68998c7c9d2076930d00654f0dde37
Kieran Simkin <kieran@slinq.com>
2012-02-21 11:19:13 +0000

    some work on the authentication process

--- controllers/controller.Auth.php
+++ controllers/controller.Auth.php
@@ -14,8 +14,69 @@ class AuthController extends OpenController {
 	function authAPI($arg,$input) { 
 		$ret=array();
 		$user=$this->ensure_api_user($input);
+		$challenge=$this->ensure_api_challenge($user,$input);
+		$this->ensure_response_authorized($user,$challenge,$input);
 		if ($this->api_validation_success()) { 
 
 		}
+		return $ret;
 	}
+	private function ensure_response_authorized($user,$challenge,$input) { 
+		if (strlen(@$input['Response'])<1) { 
+			$this->api_error(5,"Response field is required");
+			return null;
+		}
+		if (strlen(@$input['ResponseHashType')<1) { 
+			$this->api_error(6,"ResponseHashType field is required");
+			return null;
+		}
+		if (@$input['ResponseHashType']!='SHA1' && @$input['ResponseHashType']!='SHA256' && @$input['ResponseHashType']!='SHA384' && @$input['ResponseHashType']!='SHA512' && @$input['ResponseHashType']!='MD5') { 
+			$this->api_error(7,"ResponseHashType must be either 'SHA1', 'SHA256', 'SHA384', 'SHA512' or 'MD5'");
+			return null;
+		}
+		return $this->ensure_hash_match($user,$challenge,$input);
+	}
+	private function ensure_hash_match($user,$challenge,$input) { 
+		$hash=hash(strtolower($input['ResponseHashType']),$challenge->challenge.$user->password);
+		if (strtolower(@$input['Response'])!=$hash) { 
+			$this->api_error(8,"Authentication failed");
+			return null;
+		}
+		return true;
+	}
+	private function ensure_api_challenge($user,$input) { 
+		if (strlen(@$input['ChallengeID'])<1) { 
+			$this->api_error(3,"ChallengeID field is required");
+			return null;
+		}
+		$challenge=User_challenge::get($input['ChallengeID']);
+		if (is_null($challenge)||PEAR::isError($challenge)) { 
+			$this->api_error(4,"ChallengeID not found");
+			return null;
+		}
+		if ($challenge->user_id != $user->id) { 
+			$this->api_error(4,"ChallengeID not found");
+			return null;
+		}
+		return $challenge;
+	}
+	private function ensure_api_user($input) {
+		$user=null;
+		if (strlen(@$input['UserID'])<1 && strlen(@$input['Username'])<1 && strlen(@$input['Email'])<1) { 
+			$this->api_error(1,"Username, Email, or UserID field is required");
+		} else { 
+			if (strlen(@$input['UserID'])>0) { 
+				$user=User::get($input['UserID']);
+			} else if (strlen(@$input['Username'])>0) { 
+				$user=User::get($input['Username'],'username');
+			} else if (strlen(@$input['Email'])>0) { 
+				$user=User::get($input['Email'],'email');
+			}
+			if (is_null($user) || PEAR::isError($user)) { 
+				$this->api_error(2,"Username, Email or UserID not found");
+			}
+		}
+		return $user;
+	}
+
 }
