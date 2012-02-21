2c8039c3849faf1fd1591d2a99b2f3ba25620ef6
Kieran Simkin <kieran@slinq.com>
2012-02-21 13:38:01 +0000

    add login view

--- views/auth/view.login.php
+++ views/auth/view.login.php
@@ -2,7 +2,10 @@
 $this->header(_('Login'));
 var_dump($this);
 ?>
-
+<section>
+<input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
+<input type="password" name="password" id="login-password" placeholder="******">
+</section>
 <?php
 $this->footer();
 ?>
