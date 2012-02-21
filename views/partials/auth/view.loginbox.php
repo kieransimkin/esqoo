4b2920b5931f6870b9e2dd80414656d0c1b5e56b
Kieran Simkin <kieran@slinq.com>
2012-02-21 13:45:24 +0000

    test

--- views/partials/auth/view.loginbox.php
+++ views/partials/auth/view.loginbox.php
@@ -1,6 +1,6 @@
 <?php
 var_dump($this);
-<section class="login-box">
+?><section class="login-box">
 <label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
 <label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
 <input type="hidden" id="login-challenge" value="<?=$this->challenge->challenge;?>">
