9bef811ca5b7fefe0aa1e362f71ee0dadac058dd
Kieran Simkin <kieran@slinq.com>
2012-02-21 13:47:53 +0000

    fix

--- views/partials/auth/view.loginbox.php
+++ views/partials/auth/view.loginbox.php
@@ -3,6 +3,6 @@ var_dump($this);
 ?><section class="login-box">
 <label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
 <label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
-<input type="hidden" id="login-challenge" value="<?=$this->challenge->Challenge;?>">
-<input type="hidden" id="login-challenge-id" value="<?=$this->challenge->ChallengeID;?>">
+<input type="hidden" id="login-challenge" value="<?=$this->challenge['Challenge'];?>">
+<input type="hidden" id="login-challenge-id" value="<?=$this->challenge['ChallengeID'];?>">
 </section>
