456281f69e6f157624973f64881d118dd44953cf
Kieran Simkin <kieran@slinq.com>
2012-02-21 13:44:02 +0000

    add loginbox partial

--- views/auth/view.login.php
+++ views/auth/view.login.php
@@ -1,12 +1,5 @@
 <?php
 $this->header(_('Login'));
-?>
-<section class="login-box">
-<label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
-<label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
-<input type="hidden" id="login-challenge" value="<?=$this->challenge->challenge;?>">
-<input type="hidden" id="login-challenge-id" value="<?=$this->challenge->id;?>">
-</section>
-<?php
+$this->partial('partials/auth/view.loginbox.php');
 $this->footer();
 ?>
---
+++ views/partials/auth/view.loginbox.php
@@ -0,0 +1,6 @@
+<section class="login-box">
+<label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
+<label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
+<input type="hidden" id="login-challenge" value="<?=$this->challenge->challenge;?>">
+<input type="hidden" id="login-challenge-id" value="<?=$this->challenge->id;?>">
+</section>
