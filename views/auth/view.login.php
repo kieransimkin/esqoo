c61e0ec84b05d7de60002309260bf0df9ec54de6
Kieran Simkin <kieran@slinq.com>
2012-02-21 13:41:52 +0000

    add login view

--- views/auth/view.login.php
+++ views/auth/view.login.php
@@ -3,7 +3,7 @@ $this->header(_('Login'));
 ?>
 <section class="login-box">
 <label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
-<label for="login-password"><?=$_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
+<label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
 <input type="hidden" id="login-challenge" value="<?=$this->challenge->challenge;?>">
 <input type="hidden" id="login-challenge-id" value="<?=$this->challenge->id;?>">
 </section>
