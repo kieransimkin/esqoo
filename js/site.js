e7ed0ed1f0070b70a08333ee3c1fd599c043b79e
Kieran Simkin <kieran@slinq.com>
2012-02-21 14:06:31 +0000

    attempt login procedure

--- js/site.js
+++ js/site.js
@@ -1,1 +1,21 @@
- 
+function esqoo_login() { 
+	var fieldname='Username';
+	if ($('#login-identity').indexOf('@')) { 
+		fieldname='Email';
+	}
+	$.getJSON({ 
+		url: '/auth/authenticate/api',
+		{ 
+			'ResponseFormat': 'json',
+			fieldname: $('#login-identity').val(),
+			'ChallengeID': $('#login-challenge-id').val(),
+			'ResponseHashType': 'SHA256',
+			'Response': esqoo_generate_password_hash($('#login-challenge').val(),$('#login-password').val())
+		}, function(data) { 
+			console.log(data);
+		}
+	});
+}
+function esqoo_generate_password_hash(challenge,password) { 
+	return challenge+password;
+}
