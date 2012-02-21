544cc7f78f93d7438511d33d84c6926d9dab00f8
Kieran Simkin <kieran@slinq.com>
2012-02-21 13:48:23 +0000

    remove debugging

--- views/partials/auth/view.loginbox.php
+++ views/partials/auth/view.loginbox.php
@@ -1,5 +1,4 @@
 <?php
-var_dump($this);
 ?><section class="login-box">
 <label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="you@yourdomain.com">
 <label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="******">
