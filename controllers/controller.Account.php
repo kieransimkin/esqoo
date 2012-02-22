53ff9f1c2cd05d1df281d255b74e7b0b98778305
Kieran Simkin <kieran@slinq.com>
2012-02-22 17:17:42 +0000

    actually unset cookie

--- controllers/controller.Account.php
+++ controllers/controller.Account.php
@@ -2,11 +2,9 @@
 class AccountController extends LockedController { 
 	public function logoutAPI($arg='',$input=array()) { 
 		$this->user_token->delete();
-		/*
 		setcookie('UserID','',time()-3600,'/');
 		setcookie('TokenID','',time()-3600,'/');
 		setcookie('Token','',time()-3600,'/');
-		*/
 		return array();
 	}
 	public function logoutUI($arg='',$input=array()) { 
