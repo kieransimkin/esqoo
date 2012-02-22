a19efdc8723e18a7d9ef21de23cbaafcc2b43c38
Kieran Simkin <kieran@slinq.com>
2012-02-22 16:30:38 +0000

    start implement lockedcontroller

--- classes/class.LockedController.php
+++ classes/class.LockedController.php
@@ -6,18 +6,22 @@ class LockedController extends Controller {
 	}
 	private function _verifyAuth($input) { 
 		if (strlen(@$input['UserID'])<1 || strlen(@$input['TokenID'])<1 || strlen(@$input['Token'])<1) { 
+			print "got here 1";
 			$this->_redirectAuthFail();
 		}
 		try { 
 			$user=User::get($input['UserID']);
 			$usertoken=User_token::get($input['TokenID']);
 			if ($token->user_id!=$user->id) { 
+				print "got here 2";
 				$this->_redirectAuthFail();
 			}
 			if ($token->token!=$input['Token']) { 
+				print "got here 3";
 				$this->_redirectAuthFail();
 			}
 		} catch (DBSQ_Exception $e) { 
+				print "got here 4";
 			$this->_redirectAuthFail();
 		}
 		$this->user=$user;
