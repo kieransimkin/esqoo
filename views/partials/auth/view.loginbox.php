1dc77d7c341cb4e4b4fb8758185027dca1eee90e
Kieran Simkin <kieran@slinq.com>
2012-02-22 17:22:06 +0000

    use <button>

--- views/partials/auth/view.loginbox.php
+++ views/partials/auth/view.loginbox.php
@@ -2,14 +2,14 @@
 ?>
 <section class="login-box">
 	<div id="login-errors" style="display: none;"></div>
-	<label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="<?=_('you@yourdomain.com');?>">
+	<label for="login-identity"><?=_('Username or Email:')?></label><input type="email" name="identity" id="login-identity" placeholder="<?=_('you@yourdomain.com');?>" autofocus="autofocus">
 	<label for="login-password"><?=_('Password:')?></label><input type="password" name="password" id="login-password" placeholder="<?=_('******');?>">
 	<input type="hidden" id="login-challenge" value="<?=$this->challenge['Challenge'];?>">
 	<input type="hidden" id="login-forward" value="<?=$this->forward;?>">
 	<input type="hidden" id="login-challenge-id" value="<?=$this->challenge['ChallengeID'];?>">
-	<input type="button" id="login-button" value="<?=_('Login');?>" onclick="esqoo_login.login(); return false;">
+	<button id="login-button" onclick="esqoo_login.login(); return false;"><?=_('Login');?></button>
 	<noscript>
-		You will need JavaScript enabled past this point.
+		<?=_('You will need JavaScript enabled past this point.');?>
 	</noscript>
 	<script>
 	<!--
