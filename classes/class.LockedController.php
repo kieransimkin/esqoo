ddedf0330a6dfa32aaa71865c9ec009b0597f910
Kieran Simkin <kieran@slinq.com>
2012-02-22 16:23:54 +0000

    start implement lockedcontroller

--- classes/class.LockedController.php
+++ classes/class.LockedController.php
@@ -5,6 +5,8 @@ class LockedController extends Controller {
 		$this->_verifyAuth(array_merge($_COOKIE,$_POST));
 	}
 	private function _verifyAuth($input) { 
-		var_dump($input);
+		if (strlen(@$input['UserID'])<1 || strlen(@$input['TokenID'])<1 || strlen(@$input['Token'])<1) { 
+			$this->redirect('/auth/login?forward='.urlencode($_SERVER['REQUEST_URI']));
+		}
 	}
 }
