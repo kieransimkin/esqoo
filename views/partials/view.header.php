f9696727ad0c82d5d916e8ecf700cf16013f3341
Kieran Simkin <kieran@slinq.com>
2012-02-23 17:31:55 +0000

    fix

--- views/partials/view.header.php
+++ views/partials/view.header.php
@@ -10,7 +10,8 @@
 	$this->cssManager->display();
 	$theme=@$this->options['theme'];
 	if (strlen($theme)<1) { 
-		$theme='ui-darkness';
+		$theme='sunny';
+		//$theme='ui-darkness';
 	}
 	?>
 	<link rel="stylesheet" href="/css/themes/<?=$theme?>/jquery.ui.all.css" />
