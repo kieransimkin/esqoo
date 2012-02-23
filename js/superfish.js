9b2795e6ba16df0e37017e658794a5572dc0a817
Kieran Simkin <kieran@slinq.com>
2012-02-23 17:04:39 +0000

    fix

--- js/superfish.js
+++ js/superfish.js
@@ -20,6 +20,7 @@
 				var $$ = $(this), menu = getMenu($$);
 				clearTimeout(menu.sfTimer);
 				$$.showSuperfishUl().siblings().hideSuperfishUl();
+				$(this).addClass(o.hoverClass);
 			},
 			out = function(){
 				var $$ = $(this), menu = getMenu($$), o = sf.op;
@@ -29,6 +30,7 @@
 					$$.hideSuperfishUl();
 					if (o.$path.length && $$.parents(['li.',o.hoverClass].join('')).length<1){over.call(o.$path);}
 				},o.delay);	
+				$(this).removeClass(o.hoverClass);
 			},
 			getMenu = function($menu){
 				var menu = $menu.parents(['ul.',c.menuClass,':first'].join(''))[0];
