1023e8f255887ab37a36b383dcf125b2a983b6c3
Kieran Simkin <kieran@slinq.com>
2012-02-23 16:03:47 +0000

    add superfish

--- js/site.js
+++ js/site.js
@@ -13,4 +13,12 @@ $(document).ready(function() {
 	$('input[type=text], input[type=email], input[type=password], input[type=url], input[type=number], input[type=search], input[type=tel]').livequery(function() { 
 		$(this).button().addClass('esqoo-text-field');
 	});
+	 $("#nav-one").supersubs({ 
+            minWidth:    12,   // minimum width of sub-menus in em units 
+            maxWidth:    15,   // maximum width of sub-menus in em units 
+            extraWidth:  1     // extra width can ensure lines don't sometimes turn over 
+                               // due to slight rounding differences and font-family 
+        }).superfish({ hidingspeed: 250, animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
+                 speed:       150, delay: 500}).find('ul');
+
 });
