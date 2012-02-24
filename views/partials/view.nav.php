008363714f8e20f4afdc19a95fe738d824aa368e
Kieran Simkin <kieran@slinq.com>
2012-02-24 10:59:17 +0000

    lambda menu

--- views/partials/view.nav.php
+++ views/partials/view.nav.php
@@ -84,7 +84,7 @@ function render_nav_element($url,$item,$user) {
 	}
 	return $content;
 }
-$first=' lambda ui-widget-header ui-corner-all';
+$first=' lambda ui-widget-header ui-corner-bottom';
 foreach($nav as $heading => $menu) {
     	$content = '';
 	foreach($menu as $url => $item) {
