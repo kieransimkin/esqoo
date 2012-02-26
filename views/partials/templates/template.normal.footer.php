c6ad95995c775a8208632b34290de2583d860db6
Kieran Simkin <kieran@slinq.com>
2012-02-26 12:34:35 +0000

    some work on the normal template

--- views/partials/templates/template.normal.footer.php
+++ views/partials/templates/template.normal.footer.php
@@ -1,1 +1,9 @@
-</div> 
+</section>
+<footer class="normal-template-footer ui-widget-content">
+<aside>
+&copy; 2011-<?=date('Y');?> Esqoo.com
+</aside>
+<nav>
+
+</nav>
+</footer>
--- views/partials/templates/template.normal.header.php
+++ views/partials/templates/template.normal.header.php
@@ -1,4 +1,5 @@
-<div class="normal-page-container"> 
-<header>
+<header class="normal-template-header ui-widget-content">
+<?php $this->partial('partials/view.titleheading.php'); ?>
 <?php $this->partial('partials/view.nav.php'); ?>
 </header>
+<section class="normal-template-body">
