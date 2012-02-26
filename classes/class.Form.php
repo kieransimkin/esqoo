7e4c662cb92aaafaea249194956d4e9394d01f76
Kieran Simkin <kieran@slinq.com>
2012-02-26 21:04:11 +0000

    add HTML_QuickForm2

--- classes/class.Form.php
+++ classes/class.Form.php
@@ -1,5 +1,8 @@
 <?php
 require_once 'HTML/QuickForm2.php';
 class Form extends HTML_QuickForm2 { 
+	public function __construct($id, $method = 'post', $attributes = null, $trackSubmit = true) {
+		parent::__construct($id,$method,$attributes,$trackSubmit);
+	}
 
 } 
