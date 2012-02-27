e7c5279349473f4176bfbbde31aef6795829876e
Kieran Simkin <kieran@slinq.com>
2012-02-27 10:40:15 +0000

    .

--- js/esqoo_ui.js
+++ js/esqoo_ui.js
@@ -37,7 +37,7 @@ esqoo_ui.make_dialog = function(options,url,params) {
 		return;
 	}
 	esqoo_ui.dialog_singletons[url]=null;
-//	console.log(esqoo_ui.dialog_singletons);
+	console.log(esqoo_ui.dialog_singletons);
 	var dialog=$("<div></div>").dialog($.extend({
 		autoOpen: true,
 		modal: options['modal'],
@@ -45,7 +45,6 @@ esqoo_ui.make_dialog = function(options,url,params) {
 		position:'center',
 		buttons: buttons,
 		open: function() {
-			console.log($(this).parent()[0]);
 			if (options.singleton) { 
 				esqoo_ui.dialog_singletons[url]=$(this).parent()[0];
 			}
