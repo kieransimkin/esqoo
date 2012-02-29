1c1719eee924ad7702deaa1b4a7944637af6c4a5
Kieran Simkin <kieran@slinq.com>
2012-02-29 14:31:38 +0000

    some work on the quickpost dialog

--- views/partials/view.nav.php
+++ views/partials/view.nav.php
@@ -4,7 +4,7 @@ $nav=array(
 		"/" => _('Dashboard'),
 		"popup:"._('Quick Media Upload').":save,cancel,singleton:/content/quick-upload" => _('Quick Upload'),
 		"popup:"._('Quick Post').":save,cancel,singleton:/blog/quick-post" => _('Quick Post'),
-		"/website" => _('Visit My Website'),
+		"targetblank:/website" => _('Visit My Website'),
 		"/lightswitch/flick" => ('Flick Light Switch')
 	),
 	_('File') => array(
@@ -22,10 +22,12 @@ $nav=array(
 			"/page/add-page" => _('Add Page'),
 			"/blog/manage-posts" => _('Blog Posts'),
 			"/page/manage-pages" => _('Pages')
+			"/links" => _('Links')
 		)
 	),
 	_('Websites') => array(
 		"/website/manage-websites" => _('Manage')
+		"/website/menus" => _('Menus')
 	),
 	_('Account') => array(
 		"popup:"._('Account Details').":save,cancel,singleton:/account/details" => _('Account Details'),
@@ -96,7 +98,7 @@ function render_nav_element($url,$item,$user) {
 	} else if (strpos($url,"targetblank:")===0) {
 		list($tgtblank,$realurl)=explode(':',$url,2);
 		list($controller, $action) = explode("/", substr($realurl, 1));
-		$content .= "<li class=\"ui-menubar-default ui-corner-all\"><div style=\"z-index: 6000; position: relative; left: -8px; top: 2px; width: 20px; float: right;\"><div style=\"position: absolute; top: 0px; left: 0px;\"><span class=\"ui-icon ui-icon-extlink\"></span></div></div><a href=\"$realurl\" target=\"_blank\">$item... </a></li>";
+		$content .= "<li class=\"ui-menubar-default ui-corner-all\"><a href=\"$realurl\" target=\"_blank\">$item... </a><div class=\"nav-float-right\"><span class=\"ui-icon ui-icon-extlink\"></span></div></li>";
 
 	} else {
 		list($controller, $action) = explode("/", substr($url, 1));
