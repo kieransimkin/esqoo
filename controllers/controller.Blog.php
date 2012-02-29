2c487cbc54188b83607eaccd21ed1334ecd9b907
Kieran Simkin <kieran@slinq.com>
2012-02-29 12:53:13 +0000

    some work on the quickpost dialog

--- controllers/controller.Blog.php
+++ controllers/controller.Blog.php
@@ -17,6 +17,11 @@ class BlogController extends LockedController {
 			$post=null;
 		}
 		$form=$this->get_post_form($input,$post);
+		if ($form->validate()) { 
+			$this->postAPI($arg='',$input);
+			$this->showMessage(_('Blog entry posted'));
+			// Redirect to the new blog entry
+		}
 		$this->view->form=$form;
 	}
 	/*************************
@@ -27,6 +32,8 @@ class BlogController extends LockedController {
 	function quickpostDialog($arg='',$input=array()) { 
 		$form=$this->get_post_form($input,null);
 		if ($form->validate()) { 
+			$this->postAPI($arg='',$input);
+			$this->showMessage(_('Blog entry posted'));
 			return $this->formSuccess();
 		} else { 
 			return $this->formFail($form,'30%','550');
@@ -46,5 +53,12 @@ class BlogController extends LockedController {
 		$form->addElement('textarea','Content',array())->setLabel(_('Content'))->addrule('required',_('Required'));
 		return $form;
 	 }
+	/*****************************************
+	 *  ┏━┓┏━┓╻   ┏━╸╻ ╻┏┓╻┏━╸╺┳╸╻┏━┓┏┓╻┏━┓  *
+	 *  ┣━┫┣━┛┃   ┣╸ ┃ ┃┃┗┫┃   ┃ ┃┃ ┃┃┗┫┗━┓  *
+	 *  ╹ ╹╹  ╹   ╹  ┗━┛╹ ╹┗━╸ ╹ ╹┗━┛╹ ╹┗━┛  *
+	 *****************************************/
+	public function postAPI ($arg='',$input=array()) {
 
+	}
 }
