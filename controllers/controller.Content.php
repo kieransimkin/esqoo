f8fcb4e54dab0bdae84ac091fe83e959c93f98d2
Kieran Simkin <kieran@slinq.com>
2012-03-03 17:27:53 +0000

    more work on the file uploader

--- controllers/controller.Content.php
+++ controllers/controller.Content.php
@@ -35,4 +35,44 @@ class ContentController extends LockedController {
 		$form->addElement('file','upload',array('class'=>'upload-form','multiple'=>'multiple'))->setLabel(_('Select files'));
 		return $form;
 	 }
+	/*****************************************
+	 *  ┏━┓┏━┓╻   ┏━╸╻ ╻┏┓╻┏━╸╺┳╸╻┏━┓┏┓╻┏━┓  *
+	 *  ┣━┫┣━┛┃   ┣╸ ┃ ┃┃┗┫┃   ┃ ┃┃ ┃┃┗┫┗━┓  *
+	 *  ╹ ╹╹  ╹   ╹  ┗━┛╹ ╹┗━╸ ╹ ╹┗━┛╹ ╹┗━┛  *
+	 *****************************************/
+	public function uploadAPI($arg='',$input=array()) { 
+		if (!isset($input['Size']) || $input['Size']!=(int)$input['Size']) { 
+			$this->api_error(1,"Size field must be specified and must be an integer");
+		}
+		if (strlen(@$input['HashType'])<1) { 
+			$this->api_error(6,"HashType field is required");
+		}
+		if (	@$input['HashType']!='SHA1' && 
+			@$input['HashType']!='SHA256' && 
+			@$input['HashType']!='SHA384' && 
+			@$input['HashType']!='SHA512' && 
+			@$input['HashType']!='MD5') { 
+				$this->api_error(7,"HashType must be either 'SHA1', 'SHA256', 'SHA384', 'SHA512' or 'MD5'");
+		}
+		if (!isset($input['Chunk'])) { 
+			$this->api_error(2,"Chunk field is required");	
+		}
+		if (!isset($input['ChunkHash']) || strlen($input['ChunkHash'])<1) { 
+			$this->api_error(4,"ChunkHash field is required");	
+		}
+		if (!isset($input['AssetID'])) { 
+			$input['AssetID']='null';
+		}
+		if ($input['AssetID']!=='null' && $input['AssetID']!=(int)$input['AssetID']) { 
+			$this->api_error(3,"If AssetID is specified, it must be an integer or 'null'");
+		}
+		if ($this->api_validation_success()) { 
+			if (!isset($input['Name']) || strlen($input['Name'])<1) { 
+				$input['Name']='Untitled';
+			}
+			if (!isset($input['ChunkSize']) || $input['ChunkSize']==='null') { 
+				$input['ChunkSize']=$input['Size'];
+			}
+		}
+	}
 }
