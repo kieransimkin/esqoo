605a88421d54a213c31c1c00bafba5fb6ef2f4dd
Kieran Simkin <kieran@slinq.com>
2012-03-07 07:46:27 +0000

    updates

--- models/model.Album.php
+++ models/model.Album.php
@@ -1,4 +1,13 @@
 <?php
 class Album extends DBSQL { 
-
+	static function get_menu($user_id) { 
+		$res=Album::getAll('deletedate is null and user_id=?',array($user_id));
+		$ret=array();
+		if (is_array($res)) { 
+			foreach ($res as $item) { 
+				$ret[$item->id]=$item->Name;
+			}
+		}
+		return $ret;
+	}
 } 
--- schemas/base.sql
+++ schemas/base.sql
@@ -59,6 +59,7 @@ insert into ui_theme set Tag='trontastic', ui_state='both';
 insert into ui_theme set Tag='ui-darkness', ui_state='both';
 insert into ui_theme set Tag='ui-lightness', ui_state='day';
 insert into ui_theme set Tag='vader', ui_state='both';
+insert into ui_theme set Tag='purple-lady', ui_state='both';
 drop table if exists `rich_editor`;
 create table `rich_editor` (
 	id int not null auto_increment,
