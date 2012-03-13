70cd586c9e19e83c2f1721b5c8fc52ef923bd06f
Kieran Simkin <kieran@slinq.com>
2012-03-13 13:47:07 +0000

    tidy done

--- controllers/controller.Message.php
+++ controllers/controller.Message.php
@@ -1,5 +1,10 @@
 <?php
 class MessageController extends LockedController { 
+	/*****************************************
+	 *  ┏━┓┏━┓╻   ┏━╸╻ ╻┏┓╻┏━╸╺┳╸╻┏━┓┏┓╻┏━┓  *
+	 *  ┣━┫┣━┛┃   ┣╸ ┃ ┃┃┗┫┃   ┃ ┃┃ ┃┃┗┫┗━┓  *
+	 *  ╹ ╹╹  ╹   ╹  ┗━┛╹ ╹┗━╸ ╹ ╹┗━┛╹ ╹┗━┛  *
+	 *****************************************/
 	public function getAPI($arg='',$input=array()) { 
 		$messages=Message::getAll('user_id=? and CompleteDate is null',array($this->user->id));
 		DBSQL::set_all_visible_api_fields($messages,$this->get_message_fields());
@@ -26,6 +31,11 @@ class MessageController extends LockedController {
 		}
 		return $message;
 	}
+	/****************************
+	 *  ┏━┓┏━┓╻╻ ╻┏━┓╺┳╸┏━╸┏━┓  *
+	 *  ┣━┛┣┳┛┃┃┏┛┣━┫ ┃ ┣╸ ┗━┓  *
+	 *  ╹  ╹┗╸╹┗┛ ╹ ╹ ╹ ┗━╸┗━┛  *
+	 ****************************/
 	private function get_message_fields() { 
 		return array('id','CreateDate','Severity','Message','CompleteDate');
 	}
