16ec8acc826cc435a86aa337ed140994e94a1cec
Kieran Simkin <kieran@slinq.com>
2012-03-13 12:59:24 +0000

    pxem

--- js/esqoo_ui.js
+++ js/esqoo_ui.js
@@ -49,7 +49,10 @@ esqoo_ui.add_message = function (message) {
 }
 esqoo_ui.update_message_queue_positions = function() { 
 	for (var c=0; c<esqoo_ui.message_queue.length; c++) { 
-		var targetheight=$(c*6).toPx();
+		var targetheight=0;
+		if (c>0) { 
+			targetheight=$(c*6).toPx();
+		}
 		var currentheight=esqoo_ui.message_queue[c].container.css('bottom');
 		console.log('c: '+c+' targetheight: '+targetheight+' currentheight: '+currentheight);
 		esqoo_ui.message_queue[c].container.animate({bottom: targetheight},{duration: 'slow'});
