6f8c3b064805b50f1855c705a323683a119d8a4b
Kieran Simkin <kieran@slinq.com>
2012-03-16 09:47:57 +0000

    remove tinymce submodule

--- js/jquery.ui.qrichedit.js
+++ js/jquery.ui.qrichedit.js
@@ -6,7 +6,46 @@ $.widget('esqoo.qrichedit', {
 	visual_editor: 'TinyMCE',
 	code_editor: 'Ace',
 	_create: function() {
+		this._do_javascript_loads();
 		this._do_html_setup();
+	},
+	_load_javascript: function(s,callback) { 
+		var me = this;
+		var head= document.getElementsByTagName('head')[0];
+		var script= document.createElement('script');
+		script.type= 'text/javascript';
+		script.onreadystatechange= function () {
+			if (this.readyState == 'complete') me._script_load_complete(s,callback)();
+		}
+		script.onload= me._script_load_complete(s,callback);
+		script.src= s;
+		head.appendChild(script);
+	},
+	_script_load_complete: function(script,callback) { 
+		return function(e) { 
+			callback(script);
+		}
+	},
+	_do_javascript_loads: function() { 
+		switch(this.visual_editor) { 
+			case 'TinyMCE':
+			
+			break;
+			case 'CKEditor':
+
+			break;
+		}
+		switch(this.code_editor) { 
+			case 'EditArea':
+
+			break;
+			case 'Ace':
+
+			break;
+			case 'markItUp':
+
+			break;
+		}
 	},
 	_do_html_setup: function() { 
 
