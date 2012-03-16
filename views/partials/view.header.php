a9d29a09147c0154434c2acc3a39766cc0739261
Kieran Simkin <kieran@slinq.com>
2012-03-16 14:02:20 +0000

    day/night states

--- views/partials/view.header.php
+++ views/partials/view.header.php
@@ -62,5 +62,5 @@ if ((isset($this->js) && $this->js) || (isset($this->jsOnloads) && $this->jsOnlo
 	</script>
 	<script src="http://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js"></script>
 </head>
-<body class="controller-<?=strtolower($this->controller);?> action-<?=strtolower($this->controller).'-'.strtolower($this->controller->action);?> template-<?=strtolower($this->template);?>">
+<body class="controller-<?=strtolower($this->controller);?> action-<?=strtolower($this->controller).'-'.strtolower($this->controller->action);?> template-<?=strtolower($this->template);?> <?=strtolower($this->user->DayState);?>">
 
