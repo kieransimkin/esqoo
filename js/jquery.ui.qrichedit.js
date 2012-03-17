422483e15db858f4431636709b053077247aac01
Kieran Simkin <kieran@slinq.com>
2012-03-17 01:24:09 +0000

    markitup working

--- js/jquery.ui.qrichedit.js
+++ js/jquery.ui.qrichedit.js
@@ -30,6 +30,14 @@ $.widget('esqoo.qrichedit', {
 		script.src= s;
 		head.appendChild(script);
 	},
+	_load_stylesheet: function(s) { 
+		var me = this;
+		var head = document.getElementsByTagName('head')[0];
+		var link = document.createElement('link');
+		link.rel='stylesheet';
+		link.href=s;
+		head.appendChild(link);
+	},
 	_script_load_complete: function(script,callback) { 
 		return function(e) { 
 			callback(script);
@@ -76,36 +84,28 @@ $.widget('esqoo.qrichedit', {
 			break;
 			case 'markItUp':
 				this._load_javascript('/js/markitup/markitup/jquery.markitup.js',function() { 
-					$(me.codetextbox).markItUp({
-    nameSpace:       "html", // Useful to prevent multi-instances CSS conflict
-    onShiftEnter:    {keepDefault:false, replaceWith:'<br />\n'},
-    onCtrlEnter:     {keepDefault:false, openWith:'\n<p>', closeWith:'</p>\n'},
-    onTab:           {keepDefault:false, openWith:'     '},
-    markupSet:  [
-        {name:'Heading 1', key:'1', openWith:'<h1(!( class="[![Class]!]")!)>', closeWith:'</h1>', placeHolder:'Your title here...' },
-        {name:'Heading 2', key:'2', openWith:'<h2(!( class="[![Class]!]")!)>', closeWith:'</h2>', placeHolder:'Your title here...' },
-        {name:'Heading 3', key:'3', openWith:'<h3(!( class="[![Class]!]")!)>', closeWith:'</h3>', placeHolder:'Your title here...' },
-        {name:'Heading 4', key:'4', openWith:'<h4(!( class="[![Class]!]")!)>', closeWith:'</h4>', placeHolder:'Your title here...' },
-        {name:'Heading 5', key:'5', openWith:'<h5(!( class="[![Class]!]")!)>', closeWith:'</h5>', placeHolder:'Your title here...' },
-        {name:'Heading 6', key:'6', openWith:'<h6(!( class="[![Class]!]")!)>', closeWith:'</h6>', placeHolder:'Your title here...' },
-        {name:'Paragraph', openWith:'<p(!( class="[![Class]!]")!)>', closeWith:'</p>'  },
-        {separator:'---------------' },
-        {name:'Bold', key:'B', openWith:'<strong>', closeWith:'</strong>' },
-        {name:'Italic', key:'I', openWith:'<em>', closeWith:'</em>'  },
-        {name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
-        {separator:'---------------' },
-        {name:'Ul', openWith:'<ul>\n', closeWith:'</ul>\n' },
-        {name:'Ol', openWith:'<ol>\n', closeWith:'</ol>\n' },
-        {name:'Li', openWith:'<li>', closeWith:'</li>' },
-        {separator:'---------------' },
-        {name:'Picture', key:'P', replaceWith:'<img src="[![Source:!:http://]!]" alt="[![Alternative text]!]" />' },
-        {name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
-        {separator:'---------------' },
-        {name:'Clean', replaceWith:function(h) { return h.selection.replace(/<(.*?)>/g, "") } },
-        {name:'Preview', call:'preview', className:'preview' }
-    ]
+						me._load_stylesheet('/js/markitup/markitup/skins/markitup/style.css');
+						me._load_stylesheet('/js/markitup/markitup/sets/default/style.css');
+						$(me.codetextbox).markItUp({
+	onShiftEnter:  	{keepDefault:false, replaceWith:'<br />\n'},
+	onCtrlEnter:  	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>'},
+	onTab:    		{keepDefault:false, replaceWith:'    '},
+	markupSet:  [ 	
+		{name:'Bold', key:'B', openWith:'(!(<strong>|!|<b>)!)', closeWith:'(!(</strong>|!|</b>)!)' },
+		{name:'Italic', key:'I', openWith:'(!(<em>|!|<i>)!)', closeWith:'(!(</em>|!|</i>)!)'  },
+		{name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
+		{separator:'---------------' },
+		{name:'Bulleted List', openWith:'    <li>', closeWith:'</li>', multiline:true, openBlockWith:'<ul>\n', closeBlockWith:'\n</ul>'},
+		{name:'Numeric List', openWith:'    <li>', closeWith:'</li>', multiline:true, openBlockWith:'<ol>\n', closeBlockWith:'\n</ol>'},
+		{separator:'---------------' },
+		{name:'Picture', key:'P', replaceWith:'<img src="[![Source:!:http://]!]" alt="[![Alternative text]!]" />' },
+		{name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
+		{separator:'---------------' },
+		{name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/<(.*?)>/g, "") } },		
+		{name:'Preview', className:'preview',  call:'preview'}
+	]
 });
-					console.log('markItUp');
+
 				});
 			break;
 		}
@@ -189,7 +189,7 @@ $.widget('esqoo.qrichedit', {
 
 	},
 	_read_value_from_markitup: function() { 
-
+		this.current_value=this.codetextbox.val();
 	},
 	_read_value_from_raw: function() { 
 		this.current_value=this.rawtextbox.val();
@@ -231,7 +231,7 @@ $.widget('esqoo.qrichedit', {
 
 	},
 	_write_value_to_markitup: function() { 
-
+		this.codetextbox.val(this.current_value);
 	},
 	_write_value_to_raw: function() { 
 		this.rawtextbox.val(this.current_value);
