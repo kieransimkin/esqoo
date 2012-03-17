9a5b9c1e2fa94f3150a9b02842df60e49a613194
Kieran Simkin <kieran@slinq.com>
2012-03-17 00:52:19 +0000

    some work on the editor widget

--- js/jquery.ui.qrichedit.js
+++ js/jquery.ui.qrichedit.js
@@ -2,10 +2,12 @@
 $.ui.staticQRichEdit={'unique_id':0};
 $.widget('esqoo.qrichedit', {
 	options: { 
-		
+		defaulttab: 'visual'
 	},
 	unique_id:null,
+	current_tab:null,
 	_create: function() {
+		this.current_tab=this.options.defaulttab;
 		this._do_html_setup();
 		this._do_javascript_loads();
 	},
@@ -54,9 +56,15 @@ $.widget('esqoo.qrichedit', {
 		switch(esqoo_ui.code_editor) { 
 			case 'EditArea':
 				this._load_javascript('/js/editarea/edit_area_full.js', function() { 
+					/*
 					editAreaLoader.init({
-						id: $(me.codetextbox).attr('id')
+						id: $(me.codetextbox).attr('id'),
+						allow_toggle: false,
+						syntax: "html",
+						start_highlight: true
+		
 					});
+					*/
 					console.log('EditArea loaded');
 				});
 			break;
@@ -112,6 +120,7 @@ $.widget('esqoo.qrichedit', {
 					.appendTo(this.container);
 		this.visualtextbox=$('<textarea></textarea>')
 					.html(this.element.html())
+					.css({width: '100%'})
 					.attr('id','esqoo-ui-visual-text-box-'+this._get_unique_id())
 					.appendTo(this.visualtabcontainer);
 		this.codetabcontainer=$('<div></div>')
@@ -120,6 +129,7 @@ $.widget('esqoo.qrichedit', {
 					.prependTo(this.container);
 		this.codetextbox=$('<textarea></textarea>')
 					.html(this.element.html())
+					.css({width: '100%'})
 					.attr('id','esqoo-ui-code-text-box-'+this._get_unique_id())
 					.appendTo(this.codetabcontainer);
 		this.rawtabcontainer=$('<div></div>')
@@ -128,6 +138,7 @@ $.widget('esqoo.qrichedit', {
 					.prependTo(this.container);
 		this.rawtextbox=$('<textarea></textarea>')
 					.html(this.element.html())
+					.css({width: '100%'})
 					.attr('id','esqoo-ui-raw-text-box-'+this._get_unique_id())
 					.appendTo(this.rawtabcontainer);
 		this.tabbar=$('<ul></ul>')
@@ -137,8 +148,140 @@ $.widget('esqoo.qrichedit', {
 		$('<li><a href="#esqoo-ui-visual-rich-editor-'+this._get_unique_id()+'">Visual</a></li>').appendTo(this.tabbar);
 		$('<li><a href="#esqoo-ui-code-rich-editor-'+this._get_unique_id()+'">Code</a></li>').appendTo(this.tabbar);
 		$('<li><a href="#esqoo-ui-raw-rich-editor-'+this._get_unique_id()+'">Raw</a></li>').appendTo(this.tabbar);
-		this.container.tabs();
+		this.container.tabs({select: this._handle_tab_change()});
 		this.element.hide();
+	},
+	_read_value_from_visual: function() { 
+		switch (esqoo_ui.visual_editor) { 
+			case 'TinyMCE':
+				this._read_value_from_tinymce();
+			break;
+			case 'CKEditor':
+				this._read_value_from_ckeditor();	
+			break;
+		}
+	},
+	_read_value_from_tinymce: function() { 
+		console.log('got here');
+		this.current_value=this.visualtextbox.html();
+		console.log(this.current_value);
+	},
+	_read_value_from_ckeditor: function() { 
+		this.current_value=this.visualtextbox.val();
+	},
+	_read_value_from_code: function() { 
+		switch(esqoo_ui.code_editor) { 
+			case 'EditArea':
+			this._read_value_from_editarea();	
+			break;
+			case 'Ace':
+			this._read_value_from_ace();	
+			break;
+			case 'markItUp':
+			this._read_value_from_markitup();
+			break;
+		}
+	},
+	_read_value_from_editarea: function() { 
+		this.current_value=editAreaLoader.getValue($(this.codetextbox).attr('id'));
+	},
+	_read_value_from_ace: function() { 
+
+	},
+	_read_value_from_markitup: function() { 
+
+	},
+	_read_value_from_raw: function() { 
+		this.current_value=this.rawtextbox.val();
+	},
+	_write_value_to_visual: function() { 
+		switch (esqoo_ui.visual_editor) { 
+			case 'TinyMCE':
+				this._write_value_to_tinymce();
+			break;
+			case 'CKEditor':
+				this._write_value_to_ckeditor();	
+			break;
+		}
+	},
+	_write_value_to_tinymce: function() { 
+		this.visualtextbox.tinymce().execCommand('mceReplaceContent',false,this.current_value);
+	},
+	_write_value_to_ckeditor: function() { 
+		this.visualtextbox.val(this.current_value);
+	},
+	_write_value_to_code: function() { 
+		switch(esqoo_ui.code_editor) { 
+			case 'EditArea':
+			this._write_value_to_editarea();	
+			break;
+			case 'Ace':
+			this._write_value_to_ace();	
+			break;
+			case 'markItUp':
+			this._write_value_to_markitup();
+			break;
+		}
+
+	},
+	_write_value_to_editarea: function() { 
+		editAreaLoader.setValue($(this.codetextbox).attr('id'),this.current_value);
+	},
+	_write_value_to_ace: function() { 
+
+	},
+	_write_value_to_markitup: function() { 
+
+	},
+	_write_value_to_raw: function() { 
+		this.rawtextbox.html(this.current_value);
+	},
+	_handle_tab_change: function() { 
+		var me = this;
+		return function(e,ui) { 
+			var tab=null;
+			switch(me.current_tab) { 
+				case 'visual':
+				me._read_value_from_visual();
+				break;
+				case 'code':
+				me._read_value_from_code();
+				break;
+				case 'raw':
+				me._read_value_from_raw();
+				break;
+			}
+			switch (ui.index) { 
+				case 0:
+				// Visual tab
+				me._write_value_to_visual();
+				tab='visual';
+				break;
+				case 1:
+				// Code tab
+				me._write_value_to_code();
+				tab='code';
+				break;
+				case 2:
+				// Raw tab
+				me._write_value_to_raw();
+				tab='raw';
+				break;
+			}
+			if (me.current_tab=='code' && tab !='code' && esqoo_ui.code_editor=='EditArea') { 
+				me._hide_editarea();
+			}
+			if (me.current_tab!='code' && tab=='code' && esqoo_ui.code_editor=='EditArea') { 
+				me._show_editarea();
+			}
+			me.current_tab=tab;
+		}
+	},
+	_hide_editarea: function() { 
+		editAreaLoader.delete_instance($(this.codetextbox).attr('id'));
+	},
+	_show_editarea: function() { 
+		editAreaLoader.init({id:$(this.codetextbox).attr('id'),allow_toggle:false, syntax: "html", start_highlight: true });
 	},
 	_setOption: function (key, value) { 
 		switch(key) { 
