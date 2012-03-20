812d548e5a4fb7b0fea0e0cb650417fff23717aa
Kieran Simkin <kieran@slinq.com>
2012-03-20 17:02:17 +0000

    test

--- controllers/controller.Asset.php
+++ controllers/controller.Asset.php
@@ -1,7 +1,32 @@
 <?php
 class AssetController extends DetachedController { 
 	public function remap($uri,$input=array()) { 
-		var_dump(array($uri,$input));
+		if (substr($uri,0,9)=='/picture/') { 
+			$this->get_picture(substr($uri,9));
+		} else if (substr($uri,0,7)=='/video/') { 
+			$this->get_video(substr($uri,7));
+		} else if (substr($uri,0,7)=='/audio/') { 
+			$this->get_audio(substr($uri,7));
+		} else if (substr($uri,0,6)=='/file/') { 
+			$this->get_file(substr($uri,6));
+		} else { 
+			$this->get_asset($uri);	
+		}
 		die;
+	}
+	private function get_picture($uri) { 
+		print "Getting picture: $uri";
+	}
+	private function get_video($uri) { 
+		print "Getting video: $uri";
+	}
+	private function get_audio($uri) { 
+		print "Getting audio: $uri";
+	}
+	private function get_file($uri) { 
+		print "Getting file: $uri";
+	}
+	private function get_asset($uri) {
+		print "Getting Asset: $uri";
 	}
 }
