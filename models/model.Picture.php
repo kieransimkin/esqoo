1d4bf119167ad08f620bf12cbe935cbd6fc95ffe
Kieran Simkin <kieran@slinq.com>
2012-03-21 21:14:37 +0000

    image thumbnailing surprisingly easy, imagemagick rocks

--- models/model.Picture.php
+++ models/model.Picture.php
@@ -2,8 +2,13 @@
 class Picture extends DBSQL { 
 	public static $_cachedfields=array('digital_negative__asset_id','web_small__asset_id','web_medium__asset_id','web_large__asset_id','web_fullsize__asset_id','thumbnail_small__asset_id','thumbnail_large__asset_id','square__asset_id');
 	public function generate_thumbnail($size) { 
-	
-		var_dump($size);
-		var_dump($this->user->get_picture_size($size));
+		$max=$this->album->user->get_picture_size($size);
+		$image=new Imagick($this->web_fullsize__asset->get_filename());
+		$image->thumbnailImage($max,$max,true);
+		$image->setImageFormat('jpeg');
+	        $image->setImageCompressionQuality(90);
+		header('Content-type: image/jpeg');
+		echo $image;
+		die;
 	}
 } 
