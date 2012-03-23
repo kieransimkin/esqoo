ba5ba62cab7545c72b90ff15485f69c2739de06f
Kieran Simkin <kieran@slinq.com>
2012-03-23 13:24:12 +0000

    testk

--- classes/class.DBSQL.php
+++ classes/class.DBSQL.php
@@ -45,7 +45,7 @@ class DBSQL extends DBSQ {
 			foreach (static::$_cachedfields as $cachedfield) { 
 				try { 
 					$data=parent::__get($cachedfield);
-					Cache::setKey('DB-'.strtolower(get_called_class()).'-'.strtolower($this->_get_lazyLoadIndexName()), $this->_get_lazyLoadId().'-'.strtolower($cachedfield),parent::__get($cachedfield));
+					//Cache::setKey('DB-'.strtolower(get_called_class()).'-'.strtolower($this->_get_lazyLoadIndexName()), $this->_get_lazyLoadId().'-'.strtolower($cachedfield),parent::__get($cachedfield));
 				} catch (DBSQ_Exception $e) { 
 					var_dump($e);
 					//noop
