65ceedd6aee1887d5671ef2a316b55a91bb5c2ca
Kieran Simkin <kieran@slinq.com>
2012-03-23 12:58:21 +0000

    test

--- classes/class.DBSQL.php
+++ classes/class.DBSQL.php
@@ -23,7 +23,7 @@ class DBSQL extends DBSQ {
 					Site::loadAndConnect();
 					return parent::__get($key);
 				} else { 
-					print "got here";
+					print "got here".$newkey.'-'.$ret;
 					$this->$newkey=$ret;
 					return parent::__get($key);
 				}
