6299d7e2c86068f419189340e7b1cbf9f954e029
Kieran Simkin <kieran@slinq.com>
2012-03-25 20:29:58 +0100

    exif support

--- classes/class.Helper.php
+++ classes/class.Helper.php
@@ -41,5 +41,28 @@ class Helper {
 	static function url_friendly_encode($url) { 
 		return urlencode(iconv('UTF-8','ASCII//TRANSLIT',str_replace(' ','-',$url)));
 	}
+	static function getGps($exifCoord, $hemi) {
+
+	    $degrees = count($exifCoord) > 0 ? Helper::gps2Num($exifCoord[0]) : 0;
+	    $minutes = count($exifCoord) > 1 ? Helper::gps2Num($exifCoord[1]) : 0;
+	    $seconds = count($exifCoord) > 2 ? Helper::gps2Num($exifCoord[2]) : 0;
+
+	    $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+
+	    return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+
+	}
+	static function gps2Num($coordPart) {
+	    $parts = explode('/', $coordPart);
+
+	    if (count($parts) <= 0)
+		return 0;
+
+	    if (count($parts) == 1)
+		return $parts[0];
+
+	    return floatval($parts[0]) / floatval($parts[1]);
+	}
+
 
 }
