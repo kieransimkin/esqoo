973161b9f61c51953ec3b7ffd2f25339f2bf5429
Kieran Simkin <kieran@slinq.com>
2012-03-25 12:00:28 +0100

    add stubs for new javascripts

---
+++ js/jquery.ui.sqtip.js
@@ -0,0 +1,30 @@
+(function( $ ) {
+$.widget( "esqoo.sqtip", {
+	options: {
+
+	},
+	_create: function() { 
+		this._do_html_setup();
+	},
+	_do_html_setup: function() { 
+
+	},
+	// Use the _setOption method to respond to changes to options
+	_setOption: function( key, value ) {
+		switch( key ) {
+
+			case "disabled":
+			// handle changes to disabled option
+
+			break;
+
+		}
+
+		$.Widget.prototype._setOption.apply( this, arguments );
+	},
+	destroy: function() {
+		$.Widget.prototype.destroy.call( this );
+
+	}
+});
+}(jQuery)); 
--- views/partials/view.header.php
+++ views/partials/view.header.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 <?php
-	$this->cssManager->add("site","superfish","nav","jquery.ui.selectmenu","flexigrid");
+	$this->cssManager->add("site","superfish","nav","jquery.ui.selectmenu","flexigrid","jquery.qtip");
 	$this->cssManager->add("template.".$this->template);
 //	$this->cssManager->add("query.ui.base","themes/base/jquery.ui.core","themes/base/jquery.ui.dialog");
 	$this->cssManager->display();
@@ -22,7 +22,7 @@
 	<link rel="stylesheet" href="/css/themes/<?=$theme?>/jquery.ui.all.css" />
 <?php
 	$this->cssManager->display();
-	$this->jsManager->add("jquery-1.7.1.min", "jquery-ui-1.8.17.custom","modernizr-2.5.3","sha256","jquery.livequery","esqoo_helpers","esqoo_login","esqoo_ui","superfish","supersubs","jquery.ui.selectmenu","jquery.ui.uploadq","jquery.ui.combobox","flexigrid","jquery.pxem","jquery.timers","jquery.ui.qrichedit");
+	$this->jsManager->add("jquery-1.7.1.min", "jquery-ui-1.8.17.custom","modernizr-2.5.3","sha256","jquery.livequery","esqoo_helpers","esqoo_login","esqoo_ui","superfish","supersubs","jquery.ui.selectmenu","jquery.ui.uploadq","jquery.ui.combobox","flexigrid","jquery.pxem","jquery.timers","jquery.ui.qrichedit","jquery.qtip","jquery.ui.sqtip","jquery.ui.thumbnailbrowse");
 	$this->jsManager->add("site");
     	//$this->jsManager->add("jquery.livequery", "jquery.bgiframe.min", "jquery.tools.min", "flexigrid/flexigrid", "jquery.delayedobserver", "jquery.multiselects-0.3", "jquery.tmpl.1.1.1", "jquery.ui.selectmenu", "ui.checkbox", "jquery.blockUI", "ui.multiselect", "flot/jquery.flot", "flot/jquery.flot.pie", "flot/jquery.flot.selection", "flot/jquery.flot.navigate", "uploadify/swfobject", "uploadify/jquery.uploadify.v2.1.4.min", "superfish/superfish", "superfish/supersubs", "jquery.include", "jquery.timeago", "site", "ui", "tiny_mce/tiny_mce", "date");
 	$this->jsManager->display();
