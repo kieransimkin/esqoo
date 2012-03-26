b5fba546341a36708471f3ca264e8527d695cf4f
Kieran Simkin <kieran@slinq.com>
2012-03-26 15:15:16 +0100

    fix

--- js/album/index.js
+++ js/album/index.js
@@ -1,8 +1,8 @@
 var esqoo_album_index={};
 esqoo_album_index.actions= function (id,data) { 
 	var ret = $('<div></div>');
-	$('<button></button>').attr('data-icon-primary','ui-icon-pencil').html('<?=_('Manage')?>').attr('onclick','esqoo_album_index.manage('+id+'); return false;').appendTo(ret);
-	//$('<button></button>').attr('data-icon-primary','ui-icon-trash').html('<?=_('Delete')?>').attr('onclick','esqoo_album_index.delete('+id+'); return false;').appendTo(ret);
+	$('<button></button>').attr('data-icon-primary','ui-icon-pencil').html('Manage').attr('onclick','esqoo_album_index.manage('+id+'); return false;').appendTo(ret);
+	//$('<button></button>').attr('data-icon-primary','ui-icon-trash').html('Delete').attr('onclick','esqoo_album_index.delete('+id+'); return false;').appendTo(ret);
 	return ret;
 }
 esqoo_album_index.manage = function(id) { 
