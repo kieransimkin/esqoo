42fd31a4ab093c62237657faf0ab6dfd6304ffe2
Kieran Simkin <kieran@slinq.com>
2012-04-08 11:09:01 +0100

    fix

--- js/picture/index.js
+++ js/picture/index.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 	$('#AlbumID-0').change(esqoo_picture_index.album_select_change);
 	$('#TagID-0').change(esqoo_picture_index.tag_select_change);
 	$('#View-0').change(esqoo_picture_index.update_viewer);
+	esqoo_picture_index.album_select_change();
+	esqoo_picture_index.tag_select_change();
 });
 var esqoo_picture_index={};
 esqoo_picture_index.album_select_change = function(e) { 
-	var val = $(this).val();
+	var val = $('#AlbumID-0').val();
 	if (val!=='') { 
 		if (esqoo_picture_index.selectedstate==='tag') { 
 			$('#TagID-0').val('');
@@ -18,7 +20,7 @@ esqoo_picture_index.album_select_change = function(e) {
 	esqoo_picture_index.update_viewer();
 }
 esqoo_picture_index.tag_select_change = function(e) { 
-	var val=$(this).val();
+	var val=$('#TagID-0').val();
 	if (val!=='') { 
 		if (esqoo_picture_index.selectedstate==='album') { 
 			$('#AlbumID-0').selectmenu('value','');
@@ -30,7 +32,12 @@ esqoo_picture_index.tag_select_change = function(e) {
 	}
 	esqoo_picture_index.update_viewer();
 }
+esqoo_picture_index.update_in_progress = false;
 esqoo_picture_index.update_viewer = function() { 
+	if (esqoo_picture_index.update_in_progress) { 
+		return;
+	}
+	esqoo_picture_index.update_in_progress=true;
 	console.log($('#View-0').val());
 	console.log('updating viewer');
 }
