65ec2db971dfd3c21ddaa803e5be640ad4bd4b15
Kieran Simkin <kieran@slinq.com>
2012-04-08 11:41:28 +0100

    some work on positioning

--- views/picture/view.index.php
+++ views/picture/view.index.php
@@ -1,9 +1,13 @@
 <?php
 $this->header(_('Pictures'));
+?><div class="picture-list-table-container"><table class="picture-list-table"><tr><td class="page-form-header"><br style="clear: both;" /><?php
 echo $this->form;
 ?>
-<br style="clear: both;" />
-<div id="picturelist"></div>
+</td></tr><tr><td class="page-content">
+<div class="picture-list-rel-container"><div id="picturelist-1" style="z-index: 1;" class="picture-list-container"></div>
+<div id="picturelist-2" style="z-index: 2; visibility: hidden;" class="picture-list-container"></div>
+</div>
+</td></tr></table></div>
 <?php
 $this->footer();
 ?>  
