3cb4e1b9a47b7eba25f35e66516dec809673bb8b
Kieran Simkin <kieran@slinq.com>
2012-04-13 09:50:01 +0100

    some work on thumbnailbrowse

--- js/jquery.ui.thumbnailbrowse.js
+++ js/jquery.ui.thumbnailbrowse.js
@@ -1,11 +1,19 @@
 (function( $ ) {
 $.widget( "esqoo.thumbnailbrowse", {
 	options: {
-		esqoo_xml_ajax: null
+		esqoo_xml_data: null,
+		esqoo_xml_ajax: null,
+		atom_xml_data: null,
+		atom_xml_ajax: null,
+		"json_data": null,
+		"json_ajax": null,
+		"flickr_public_photos_data": null,
+		"flickr_favorites_data": null,
+		"flickr_groups_data": null
 	},
 	_create: function() { 
 		this._do_html_setup();
-		console.log(this.options);
+		this._init_data();
 	},
 	_do_html_setup: function() { 
 		this.container=$('<div></div>')
@@ -89,6 +97,181 @@ $.widget( "esqoo.thumbnailbrowse", {
 		}
 
 		$.Widget.prototype._setOption.apply( this, arguments );
+	},
+	// Load and parse the data from its data source
+	_init_data: function() { 
+		o=this;
+		if (this.options.atom_xml_data !== null) { 
+			console.log('1');
+			this.dataType='atom';
+			if (typeof(this.options.atom_xml_data)=='string') { 
+				this.data=$.parseXML(this.options.atom_xml_data);
+			} else { 
+				this.data=this.options.atom_xml_data;
+			}
+			this._init_display();
+		} else if (this.options.atom_xml_ajax !== null) { 
+			if (typeof(this.options.atom_xml_ajax)!='string') { 
+				$.ajax(this.options.atom_xml_ajax.url,{data: this.options.atom_xml_ajax.options, success: function(data) { 
+					o.data=$(data);
+					o.dataType='atom';
+					o._init_display();
+				}, error: function(j,t,e) { 
+					throw new Error('ThumbnailBrowse: '+t);
+				}});
+			} else { 
+				$.ajax(this.options.atom_xml_ajax,{success: function(data) { 
+					o.data=$(data);
+					o.dataType='atom';
+					o._init_display();
+				}, error: function(j,t,e) { 
+					throw new Error('ThumbnailBrowse: '+t);
+				}});
+			}
+		} else if (this.options.esqoo_xml_data !== null) { 
+			this.dataType='esqoo';
+			if (typeof(this.options.esqoo_xml_data)=='string') { 
+				this.data=$.parseXML(this.options.esqoo_xml_data);
+			} else { 
+				this.data=this.options.esqoo_xml_data;
+			}
+			this._init_display();
+		} else if (this.options.esqoo_xml_ajax !== null) { 
+			console.log('got here');
+			if (typeof(this.options.esqoo_xml_ajax)!='string') { 
+				$.ajax(this.options.esqoo_xml_ajax.url,{data: this.options.esqoo_xml_ajax.options, success: function(data) { 
+					o.data=$(data);
+					o.dataType='esqoo';
+					o._init_display();	
+				}, error: function(j,t,e) { 
+					throw new Error('ThumbnailBrowse: '+t);
+				}});
+			} else { 
+				$.ajax(this.options.esqoo_xml_ajax,{success: function(data) { 
+					o.data=$(data);
+					o.dataType='esqoo';
+					o._init_display();
+				}, error: function(j,t,e) { 
+					throw new Error('ThumbnailBrowse: '+t);
+				}});
+			}
+		} else if (this.options.json_data!== null) { 
+			this.dataType='json';
+			if (typeof(this.options.json_data)=='string') { 
+				this.data=$.parseJSON(this.options.json_data);
+			} else { 
+				this.data=this.options.json_data;
+			}
+			this._init_display();
+		} else if (this.options.json_ajax !== null) { 
+			if (typeof(this.options.json_ajax)!='string') { 
+				$.getJSON(this.options.json_ajax.url,this.options.json_ajax.options, function(data) { 
+					o.data=$(data);
+					o.dataType='json';
+					o._init_display();
+				});
+			} else { 
+				$.getJSON(this.options.json_ajax,{}, function(data) { 
+					o.data=$(data);
+					o.dataType='json';
+					o._init_display();
+				});
+			}
+		} else if (this.options.flickr_public_photos_data !== null) { 
+			if (typeof(this.options.flickr_public_photos_data)!='string') { 
+				this.flickr_smallthumbs=this.options.flickr_public_photos_data.smallthumbs;
+				this.flickr_largenormals=this.options.flickr_public_photos_data.largenormals;
+				this.flickr_smallnormals=this.options.flickr_public_photos_data.smallnormals;
+				$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",{id: this.options.flickr_public_photos_data.id, ids: this.options.flickr_public_photos_data.ids, tags: this.options.flickr_public_photos_data.tags, tagmode: this.options.flickr_public_photos_data.tagmode, format: 'json'}, function(data) { 
+					o.data=data;
+					o.dataType='flickr';
+					o._init_display();	
+				});
+			} else { 
+				$.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?",{id: this.options.flickr_public_photos_data, format: 'json'}, function(data) { 
+					o.data=data;
+					o.dataType='flickr';
+					o._init_display();	
+				});
+			}
+		} else if (this.options.flickr_favorites_data !== null) { 
+			if (typeof(this.options.flickr_favorites_data)!='string') { 
+				this.flickr_smallthumbs=this.options.flickr_favorites_data.smallthumbs;
+				this.flickr_largenormals=this.options.flickr_favorites_data.largenormals;
+				this.flickr_smallnormals=this.options.flickr_favorites_data.smallnormals;
+				$.getJSON("http://api.flickr.com/services/feeds/photos_faves.gne?jsoncallback=?",{id: this.options.flickr_favorites_data.id,'format': 'json'}, function (data) { 
+					o.data=data;
+					o.dataType='flickr';
+					o._init_display();
+				});
+			} else { 
+				$.getJSON("http://api.flickr.com/services/feeds/photos_faves.gne?jsoncallback=?",{id: this.options.flickr_favorites_data, format: 'json'}, function (data) { 
+					o.data=data;
+					o.dataType='flickr';
+					o._init_display();
+				});
+			}
+		} else if (this.options.flickr_groups_data !== null) {
+			if (typeof(this.options.flickr_groups_data)!='string') { 
+				this.flickr_smallthumbs=this.options.flickr_groups_data.smallthumbs;
+				this.flickr_largenormals=this.options.flickr_groups_data.largenormals;
+				this.flickr_smallnormals=this.options.flickr_groups_data.smallnormals;
+				$.getJSON("http://api.flickr.com/services/feeds/groups_pool.gne?jsoncallback=?",{id: this.options.flickr_groups_data.id, format: 'json'}, function (data) { 
+					o.data=data;
+					o.dataType='flickr';
+					o._init_display();
+				});	
+			} else { 
+				$.getJSON("http://api.flickr.com/services/feeds/groups_pool.gne?jsoncallback=?",{id: this.options.flickr_groups_data, format: 'json'}, function (data) { 
+					o.data=data;
+					o.dataType='flickr';
+					o._init_display();
+				});
+			}
+		} else {
+			throw new Error('ThumbnailBrowse: No data specified.');
+		}
+	},
+	// Setup display
+	_init_display: function() { 
+		this._parse_data();
+		this.setup=true;
+		this._trigger('ready');
+	},
+	// Doesn't actually parse the data - that's already done, what it does is take the parsed data and translate it into a normalized format
+	_parse_data: function() { 
+		var d=new Array();
+		var me = this;
+		if (this.dataType=='atom') { 
+			this.d=d;
+		} else if (this.dataType=='esqoo') { 
+			this.data.find('Item').each(function(i,ob) { 
+				d.push({title: $(ob).find('Name').text(),
+					link: '',
+					id: $(ob).find('PictureID').text(),
+					updated: $(ob).find('ModifyDate').text(),
+					'web-fullsize': $(ob).find('PictureURLs').find('web-fullsize').text(),
+					'web-small': $(ob).find('PictureURLs').find('web-small').text(),
+					'web-medium': $(ob).find('PictureURLs').find('web-medium').text(),
+					'web-large': $(ob).find('PictureURLs').find('web-large').text(),
+					'thumbnail-large': $(ob).find('PictureURLs').find('thumbnail-large').text(),
+					'thumbnail-small': $(ob).find('PictureURLs').find('thumbnail-small').text(),
+					'square': $(ob).find('PictureURLs').find('square').text()
+				});
+				
+			});
+			this.d=d;
+		} else if (this.dataType=='json') { 
+			this.d=d;
+		} else if (this.dataType=='flickr') { 
+			this.d=d;
+		} else {
+			throw new Error('ThumbnailBrowse: unknown data type: '+this.dataType);
+		}
+		this._do_thumbnail_html_setup();
+	},
+	_do_thumbnail_html_setup: function() { 
+		console.log(this.d);
 	},
 	destroy: function() {
 		$.Widget.prototype.destroy.call( this );
