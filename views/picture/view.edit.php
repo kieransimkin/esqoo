3518bb9828a402cdae59260d30886603fd330be6
Kieran Simkin <kieran@slinq.com>
2012-05-12 21:24:39 +0100

    add fullpage CSS for edit pic page

--- views/picture/view.edit.php
+++ views/picture/view.edit.php
@@ -1,5 +1,15 @@
 <?php
 $this->header(_('Edit Picture'));
 echo $this->form;
+?>
+<div class="edit-picture-table-container"><table class="edit-picture-table" cellpadding="0" cellspacing="0"><tr><td class="page-form-header"><br style="clear: both;" /><?php
+echo $this->form;
+?>
+</td></tr><tr><td class="page-content">
+<div class="edit-picture-rel-container">
+<div id="edit-picture-container" class="edit-picture-container"></div>
+</div>
+</td></tr></table></div>
+<?php
 $this->footer();
 ?>  
