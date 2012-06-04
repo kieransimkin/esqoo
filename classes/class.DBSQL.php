f46d21d117553217a126a82b3812ebbd81570ea8
Kieran Simkin <kieran@digital-crocus.com>
2012-06-04 15:03:18 +0100

    wicked, DB models successfully namespaced

--- classes/class.DBSQL.php
+++ classes/class.DBSQL.php
@@ -1,5 +1,6 @@
 <?php
 require_once(dirname(__FILE__).'/dbsq/dbsq.class.php');
+DBSQ::setClassNamePrefix('SQ_');
 class DBSQL extends DBSQ { 
 	private $_visiblefields=array();
 	private $_computedfields=array();
