0817e53546ca54d6305858f726b21827a93014d7
Kieran Simkin <kieran@digital-crocus.com>
2012-06-16 11:50:51 +0100

    bugfix

--- models/model.Ui_theme.php
+++ models/model.Ui_theme.php
@@ -2,10 +2,10 @@
 class SQ_Ui_theme extends SQ_Class_DBSQ { 
 	static function get_menu($daystate) { 
 		if ($daystate=='Nighttime') { 
-			$res=UI_theme::getAll('ui_state=\'night\' or ui_state=\'both\'');
+			$res=SQ_Ui_theme::getAll('ui_state=\'night\' or ui_state=\'both\'');
 		} else if ($daystate=='Daytime') { 
 
-			$res=UI_theme::getAll('ui_state=\'day\' or ui_state=\'both\'');
+			$res=SQ_Ui_theme::getAll('ui_state=\'day\' or ui_state=\'both\'');
 		} else { 
 			throw new Exception('argument must be either night time or day time');
 			return;
