9246760d822fb3a2064b06b30518e58986d3c471
Kieran Simkin <kieran@digital-crocus.com>
2012-06-18 15:46:46 +0100

    Minor addition to plugin class

--- classes/class.Plugin.php
+++ classes/class.Plugin.php
@@ -2,6 +2,14 @@
 class SQ_Class_Plugin extends SQ_Class { 
 	private $identifier=null;
 	private $xml=null;
+	private static $cache=[];
+	// ::get does a cached lookup
+	public static function get($identifier) { 
+		if (array_key_exists($identifier,self::$cache)) { 
+			return self::$cache[$identifier];
+		}
+		return self::$cache[$identifier]=new SQ_Class_Plugin($identifier);
+	}
 	function __construct($identifier) { 
 		$this->identifier=$identifier;
 		$this->parseXML();
