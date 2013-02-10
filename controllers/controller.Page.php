2b589d7caa0d1e7bdf5da025a23d12de05cf131a
Kieran Simkin <kieran@digital-crocus.com>
2013-02-10 12:28:36 +0000

    some work on pages

--- controllers/controller.Page.php
+++ controllers/controller.Page.php
@@ -5,7 +5,110 @@ class SQ_Controller_Page extends SQ_Class_LockedController {
 	 *  ┃ ┃┗━┓┣╸ ┣┳┛   ┃┃┗┫ ┃ ┣╸ ┣┳┛┣╸ ┣━┫┃  ┣╸   *
 	 *  ┗━┛┗━┛┗━╸╹┗╸   ╹╹ ╹ ╹ ┗━╸╹┗╸╹  ╹ ╹┗━╸┗━╸  *
 	 **********************************************/
+	function addUI($arg='',$input=array()) { 
+		$page=$this->get_page($arg);
+		$form=$this->get_page_form($input,$page);
+		$form->addElement('submit','submit',array('data-page-publish-button'=>'true'))->setValue('Publish');
+		if ($form->validate()) { 
+			$this->addAPI($arg,$input);
+			$this->showMessage(_('Page Added'));
+			// Redirect to the new page
+		}
+		$this->view->form=$form;
+	}
 	function indexUI($arg='',$input=array()) { 
 		return;
+	}
+	/*************************
+	 *  ╺┳┓╻┏━┓╻  ┏━┓┏━╸┏━┓  *
+	 *   ┃┃┃┣━┫┃  ┃ ┃┃╺┓┗━┓  *
+	 *  ╺┻┛╹╹ ╹┗━╸┗━┛┗━┛┗━┛  *
+	 *************************/
+	function quickaddDialog($arg='',$input=array()) { 
+		$form=$this->get_page_form($input,null);
+		if ($form->validate()) { 
+			$this->addAPI($arg,$input);
+			$this->showMessage(_('Page Addded'));
+			return $this->formSuccess();
+		} else { 
+			return $this->formFail($form,'30%','550');
+		}
+	}
+	/*********************
+	 *  ┏━╸┏━┓┏━┓┏┳┓┏━┓  *
+	 *  ┣╸ ┃ ┃┣┳┛┃┃┃┗━┓  *
+	 *  ╹  ┗━┛╹┗╸╹ ╹┗━┛  *
+	 *********************/
+	 private function get_page_form($input,$post,$forcesubmit=false) { 
+		$form=new SQ_Class_Form('page');
+		$form->setAPIDataSources($input,$post,$forcesubmit);
+		$form->addElement('text','Title',array())->setLabel(_('Title'))->addRule('required',_('Required'));
+		$form->addElement('textarea','Content',array('class'=>'esqoo-qrichedit'))->setLabel(_('Content'))->addrule('required',_('Required'));
+		return $form;
+	 }
+	/*****************************************
+	 *  ┏━┓┏━┓╻   ┏━╸╻ ╻┏┓╻┏━╸╺┳╸╻┏━┓┏┓╻┏━┓  *
+	 *  ┣━┫┣━┛┃   ┣╸ ┃ ┃┃┗┫┃   ┃ ┃┃ ┃┃┗┫┗━┓  *
+	 *  ╹ ╹╹  ╹   ╹  ┗━┛╹ ╹┗━╸ ╹ ╹┗━┛╹ ╹┗━┛  *
+	 *****************************************/
+	public function addAPI($arg='',$input=array()) {
+		$oldpage=$this->get_page($input['PageID']);
+		$form=$this->get_page_form($input,$oldpage,true);
+		if (!$form->validate()) { 
+			$this->api_form_validation_error($form);
+		}
+		if ($this->api_validation_success()) { 
+			$page=SQ_Page::get();
+			$page->user_id=$this->user->id;
+			$page->Title=$input['Title'];
+			$page->Content=$input['Content'];
+			if ($oldpage) { 
+				$page->parent__page=$oldpage->id;
+			}
+			$page->save();
+			return $page;
+		}
+	}
+	public function listAPI($arg='',$input=array()) { 
+		$suffix=SQ_Class_DBSQ::getSqlSuffix($input);
+		if ($input['SearchField']=='Title' && strlen($input['SearchQuery'])>0) { 
+			$pages=SQ_Page::getAll('user_id=? and AND DeleteDate is null and Title like ?',array($this->user->id,'%'.$input['SearchQuery'].'%'),null,$suffix);
+		} else { 
+			$pages=SQ_Page::getAll('user_id=? AND DeleteDate is null',array($this->user->id),null,$suffix);
+		}
+		SQ_Class_DBSQ::set_all_visible_api_fields($pages,$this->get_page_fields());
+		$numrows=SQ_Class_DBSQ::foundRows();
+		return $this->flexigridResponse($pages,$input['Page'],$numrows);
+	}
+	public function getAPI($arg='',$input=array()) { 
+		$page=$this->get_page($input['PageID']);
+		if ($page!=null) { 
+			$page->set_visible_api_fields($this->get_page_fields());
+		}
+		return $page;
+	}
+	/****************************
+	 *  ┏━┓┏━┓╻╻ ╻┏━┓╺┳╸┏━╸┏━┓  *
+	 *  ┣━┛┣┳┛┃┃┏┛┣━┫ ┃ ┣╸ ┗━┓  *
+	 *  ╹  ╹┗╸╹┗┛ ╹ ╹ ╹ ┗━╸┗━┛  *
+	 ****************************/
+	private function get_page_fields() { 
+		return array("Title","id","Content","CreateDate","PublishDate","ModifyDate","DeleteDate");
+	}
+	private function get_page($arg) { 
+		if (strlen($arg)>0) { 
+			try {
+				$page=SQ_Page::get($arg,'id','row',true);
+				if ($page->user_id!=$this->user->id) { 
+					$page=null;
+				}
+			} catch (DBSQ_Exception $e) { 
+				// Post not found
+				$page=null;
+			}
+		} else {
+			$page=null;
+		}
+		return $page;
 	}
 }
--- views/page/view.index.php
+++ views/page/view.index.php
@@ -2,7 +2,25 @@
 $this->header(_('Pages'));
 ?>
 <h1><?=_('Pages')?></h1>
+<button data-icon-primary="ui-icon-plus" onclick="esqoo_ui.make_dialog({title:'<?=_('Quick Add');?>'},'/page/quick-add');"><?=_('Quick Add');?></button>
+<br>&nbsp;<br>
+<div class="esqoo-flexigrid" id="pagelist"
+	data-flexigrid-url="/page/list/api"
+	data-flexigrid-id-field="PageID"
+	data-flexigrid-usepager="true"
+	data-flexigrid-col0-name="Title" 
+	data-flexigrid-col0-display="<?=_('Title');?>"
+	data-flexigrid-col0-width="30%"
 
+	data-flexigrid-col1-name="Actions" 
+	data-flexigrid-col1-display="<?=_('Actions');?>"	
+	data-flexigrid-col1-width="70%"
+	data-flexigrid-col1-sortable="false"
+
+	data-flexigrid-searchitem0-name="Title"
+	data-flexigrid-searchitem0-display="<?=_('Title');?>"
+	data-flexigrid-searchitem0-isdefault="true"
+></div>
 <?php
 $this->footer();
-?> 
+?>
